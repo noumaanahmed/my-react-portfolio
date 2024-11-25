56511d3d6015295d1ebb74d4bfb1885d333e5bce
noumaanahmed <noumaan.ahmed@outlook.com>
2024-11-25 15:21:34 -0500

    Changed icon ease style and the hover effect

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -26,68 +26,37 @@ import {
 } from "react-icons/si";
 
 function Techstack() {
+  const techs = [
+    { icon: <DiJava />, name: "Java" },
+    { icon: <SiSpringboot />, name: "Spring Boot" },
+    { icon: <SiHibernate />, name: "Hibernate" },
+    { icon: <DiJavascript1 />, name: "JavaScript" },
+    { icon: <SiTypescript />, name: "TypeScript" },
+    { icon: <DiReact />, name: "React" },
+    { icon: <SiNextdotjs />, name: "Next.js" },
+    { icon: <DiNodejs />, name: "Node.js" },
+    { icon: <DiMongodb />, name: "MongoDB" },
+    { icon: <SiPostgresql />, name: "PostgreSQL" },
+    { icon: <SiMysql />, name: "MySQL" },
+    { icon: <SiDocker />, name: "Docker" },
+    { icon: <SiKubernetes />, name: "Kubernetes" },
+    { icon: <SiTerraform />, name: "Terraform" },
+    { icon: <SiAmazonaws />, name: "AWS" },
+    { icon: <SiGooglecloud />, name: "Google Cloud" },
+    { icon: <SiApachekafka />, name: "Kafka" },
+    { icon: <SiMqtt />, name: "MQTT" },
+    { icon: <DiPython />, name: "Python" },
+    { icon: <DiGit />, name: "Git" },
+  ];
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSpringboot />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiHibernate />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiTypescript />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMysql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiDocker />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiKubernetes />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiTerraform />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAmazonaws />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiGooglecloud />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiApachekafka />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMqtt />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
+      {techs.map((tech, index) => (
+        <Col xs={4} md={2} className="tech-icons" key={index}>
+          {tech.icon}
+          <span className="tech-name">{tech.name}</span>
+        </Col>
+      ))}
     </Row>
   );
 }
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -17,47 +17,30 @@ import {
 } from "react-icons/si";
 
 function Toolstack() {
+  const tools = [
+    { icon: <SiVisualstudiocode />, name: "VS Code" },
+    { icon: <SiIntellijidea />, name: "IntelliJ IDEA" },
+    { icon: <SiPostman />, name: "Postman" },
+    { icon: <SiSlack />, name: "Slack" },
+    { icon: <SiJira />, name: "JIRA" },
+    { icon: <SiFigma />, name: "Figma" },
+    { icon: <SiTerraform />, name: "Terraform" },
+    { icon: <SiDocker />, name: "Docker" },
+    { icon: <SiJenkins />, name: "Jenkins" },
+    { icon: <SiApachetomcat />, name: "Apache Tomcat" },
+    { icon: <SiPowershell />, name: "PowerShell" },
+    { icon: <SiVercel />, name: "Vercel" },
+    { icon: <SiMacos />, name: "macOS" },
+  ];
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiIntellijidea />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiJira />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFigma />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiTerraform />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiDocker />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiJenkins />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiApachetomcat />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPowershell />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
-      </Col>
+      {tools.map((tool, index) => (
+        <Col xs={4} md={2} className="tech-icons" key={index}>
+          {tool.icon}
+          <span className="tech-name">{tool.name}</span>
+        </Col>
+      ))}
     </Row>
   );
 }
